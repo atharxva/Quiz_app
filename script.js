bb0af420c86757eaf350eb969c09023b9e3f96ee
atharxva <2023.atharvas@isu.ac.in>
2024-05-03 23:22:40 +0530

    Css complete

---
+++ script.js
@@ -0,0 +1,36 @@
+const questions = [
+    {
+        questions: "Which is the largest animal on the planet",
+        answers:[
+            {text:"Shark",correct:false},
+            {text:"Blue Whale",correct:true},
+            {text:"Elephant",correct:false},
+            {text:"Giraffe",correct:false}
+
+        ]
+    },
+    {
+        question: "Who is the prime minister of india?", answers: [
+            { text: "Narendra Modi", correct: true}, 
+            { text: "Rahul Gandhi", correct: false},
+            { text: "Draupadi Murmu",correct: false},
+            { text: "Manmohan Singh", correct: false},
+        ]
+    },
+    {
+        question: "National Bird of india", answers: [
+            { text: "Parrot", correct: false}, 
+            { text: "Hornbill", correct: false},
+            { text: "Peacock",correct: true},
+            { text: "Seagull", correct: false},
+        ]
+    },
+    {
+        question: "What is the capital of India", answers: [
+            { text: "Mumbai", correct: false}, 
+            { text: "Delhi", correct: true},
+            { text: "Goa",correct: false},
+            { text: "Chennai", correct: false},
+        ]
+    },
+]
